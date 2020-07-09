f6cc1bf7bc9d3ad45b35f652a04c21d43bfedfa5
root <manudeepgadde01@gmail.com>
2020-07-09 18:15:13 +0000

    Add Default Shelf value for all books

--- src/Book.js
+++ src/Book.js
@@ -2,16 +2,26 @@ import React from 'react'
 
 class Book extends React.Component{
 	render(){
-        const {book,key} = this.props
+        const {book,books} = this.props
+        //For seacrchPage books, default shelf would be none if book is not in any shelf
+        let bookShelf = 'none'
+        // For each incoming book, if book id matches any of the booksid, shelf is set to corresponding shelf
+        for (let item of books) {
+      		if (item.id === book.id) {
+        		bookShelf = item.shelf;
+        		break;
+      		}
+    	}
+             
     	return (
-        	<li key={key}>
+        	<li>
 			  <div className='book'>
                 <div className="book-top">
                   <div className="book-cover"
                     style={{width: 128,height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}>	
                   </div>
                   <div className="book-shelf-changer">
-                      <select value={book.shelf}>
+                      <select defaultValue={bookShelf}>
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
